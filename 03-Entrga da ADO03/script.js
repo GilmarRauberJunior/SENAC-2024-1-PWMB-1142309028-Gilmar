a3efd8c52d196ff22b7315f87d3febbffcc1fa20
GILMAR RAUBER JUNIOR <gilmar.rjunior@senacsp.edu.br>
2024-04-22 09:07:09 -0300

    Atualização

--- 03-Entrga da ADO03/script.js
+++ 03-Entrga da ADO03/script.js
@@ -1,48 +1,42 @@
 function identificar() {
-    var ano = document.getElementById("ano-nasc").value;
+    var data = new Date();
+    var anoAtual = data.getFullYear();
+    var conatinerImagem = window.document.getElementById('divImg');
+    var ano = document.getElementById('ano-nasc');
 
-    var idade = 2024 - ano;
-    if(ano > 1900 && ano < 2025){
+    if (ano.value < 1900 || ano.value > anoAtual) {
         window.alert("Ano inválido.");
-    }
-    console.log(idade);
-
-    var genero = document.getElementById("genero").value;
-    var imagem = window.document.getElementById("imagem");
-    var conatinerImagem = window.document.getElementById("divImg");
-
-    switch (genero) {
-        case "Masculino":
-            conatinerImagem.style.backgroundColor = "blue";
+    } else {
+        var imagem = document.createElement('img');
+        imagem.setAttribute('id', 'imagem');
+        var genero = document.getElementsByName('genero');
+        var sexo = '';
+        var idade = anoAtual - Number(ano.value);
+        if (genero[0].checked) {
+            sexo = "masculino";
             if (idade <= 10 && idade >= 0) {
-                imagem.src = "https://img.freepik.com/fotos-gratis/retrato-de-menino_23-2150773106.jpg?size=626&ext=jpg&ga=GA1.1.337094557.1713735370&semt=ais";
+                imagem.setAttribute('src', 'https://img.freepik.com/fotos-gratis/retrato-de-menino_23-2150773106.jpg?size=626&ext=jpg&ga=GA1.1.337094557.1713735370&semt=ais');
             } else if (idade <= 25 && idade > 10) {
-                imagem.src = "https://img.freepik.com/fotos-gratis/retrato-de-um-adolescente-sorridente_171337-893.jpg";
+                imagem.setAttribute('src', 'https://img.freepik.com/fotos-gratis/retrato-de-um-adolescente-sorridente_171337-893.jpg');
             } else if (idade <= 60 && idade > 25) {
-                imagem.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnCuqo7hOuzhY5GmRqtfruNCHLiBsHU8O8wjQLF4W1vw&s";
+                imagem.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnCuqo7hOuzhY5GmRqtfruNCHLiBsHU8O8wjQLF4W1vw&s');
             } else if (idade > 60) {
-                imagem.src = "https://img.freepik.com/fotos-premium/homem-idoso-feliz_256588-850.jpg";
+                imagem.setAttribute('src', 'https://img.freepik.com/fotos-premium/homem-idoso-feliz_256588-850.jpg');
             }
-            break;
-        
-        case "Feminino":
-            conatinerImagem.style.backgroundColor = "pink";
+        } else if (genero[1].checked) {
+            sexo = "feminino";
             if (idade <= 10 && idade >= 0) {
-                imagem.src = "https://img.freepik.com/fotos-gratis/linda-garotinha-loira-sorri-piscando-posando-tocando-o-rosto-com-as-maos-no-vestido-rosa-fofo-crianca-olhando-feliz-e-encantada-copie-o-espaco_176420-10150.jpg";
-                conatinerImagem.appendChild(imagem);
+                imagem.setAttribute('src', 'https://img.freepik.com/fotos-gratis/linda-garotinha-loira-sorri-piscando-posando-tocando-o-rosto-com-as-maos-no-vestido-rosa-fofo-crianca-olhando-feliz-e-encantada-copie-o-espaco_176420-10150.jpg');
             } else if (idade <= 25 && idade > 10) {
-                imagem.src = "https://img.freepik.com/fotos-gratis/a-mulher-bonita-que-veste-o-revestimento-ocasional-da-sarja-de-nimes-sobre-a-parede-isolada-entrega-junto-e-os-dedos-cruzados-sorrindo-relaxado-e-alegre-sucesso-e-otimista_231208-1256.jpg";
-                conatinerImagem.appendChild(imagem);
+                imagem.setAttribute('src', 'https://img.freepik.com/fotos-gratis/a-mulher-bonita-que-veste-o-revestimento-ocasional-da-sarja-de-nimes-sobre-a-parede-isolada-entrega-junto-e-os-dedos-cruzados-sorrindo-relaxado-e-alegre-sucesso-e-otimista_231208-1256.jpg');
             } else if (idade <= 60 && idade > 25) {
-                imagem.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHyQ143lc1_w4X8FjZXLpWPbbtGh27gDXPd9GNru3E_w&s";
-                conatinerImagem.appendChild(imagem);
+                imagem.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHyQ143lc1_w4X8FjZXLpWPbbtGh27gDXPd9GNru3E_w&s');
             } else if (idade > 60) {
-                imagem.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm3ImXK4AKEZzISFhaRobGHau-1TE7A_BXBlrm8u1jJQ&s";
-                conatinerImagem.appendChild(imagem);
+                imagem.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm3ImXK4AKEZzISFhaRobGHau-1TE7A_BXBlrm8u1jJQ&s');
             }
-            break;
-
-        default:
-            break;
+        }
     }
+
+    conatinerImagem.innerHTML = `A pessoa identificada é do gênero ${sexo} com ${idade} anos.`;
+    conatinerImagem.appendChild(imagem);
 }
