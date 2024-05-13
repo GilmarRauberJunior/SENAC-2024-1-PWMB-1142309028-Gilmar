80405e67a670ed936a3b402cbd18e10a4bb4712a
GILMAR RAUBER JUNIOR <gilmar.rjunior@senacsp.edu.br>
2024-05-13 11:30:20 -0300

    Atualização

--- Aulas/Criador de task/script.js
+++ Aulas/Criador de task/script.js
@@ -1,11 +1,7 @@
-function createTask(){
-    const task = document.getElementById("task").value;
-    const checkTask = document.createElement("li");
-    if(task.length < 10){
-        alert("Digite uma tarefa válida");
-    }else{
-        checkTask.className = "listTask";
-        checkTask.innerText = task;
-        document.getElementById("tasksContainer").appendChild(checkTask);
-    }
+function create() {
+    console.log("Alo");
+}
+
+function list() {
+    console.log("tchau");
 }
